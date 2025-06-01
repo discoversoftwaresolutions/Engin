922619f831f1b615651f262b0eff8849e8f76cb1
Stephen Allen <founder@discoversoftwaresolution.com>
2025-05-31 21:38:46 -0400

    Create config.py

---
+++ shared/config.py
@@ -0,0 +1,12 @@
+# shared/config.py
+
+module_map = {
+    "AeroIQ – Aerospace": "modules.aeroiq",
+    "FlowCore – Digital Twin & Compliance": "modules.flowcore",
+    "FusionX – Energy & Plasma": "modules.fusionx",
+    "Simulai – Simulation AI": "modules.simulai",
+    "VisuAI – Visual Intelligence": "modules.visuai",
+    "ProtoPrint – Additive MFG": "modules.protoprint",
+    "CircuitIQ – Electronics": "modules.circuitiq",
+    "CodeMotion – Robotics Code": "modules.codemotion"
+}
