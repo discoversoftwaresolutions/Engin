64a6a7b0585ff2b6741a30d1ef6a37011a74f363
Stephen Allen <founder@discoversoftwaresolution.com>
2025-06-01 10:27:01 -0400

    Update protoprint.py

--- modules/protoprint.py
+++ modules/protoprint.py
@@ -2,8 +2,7 @@ import streamlit as st
 import logging
 import requests
 
-# ✅ Must be the first Streamlit command
-st.set_page_config(page_title="ProtoPrint – Additive Manufacturing", layout="wide")
+
 
 # ✅ Setup Logger
 logging.basicConfig(level=logging.INFO)
