4eb41d6a9cf192f0b44ff8d80f53bff926b14727
Stephen Allen <founder@discoversoftwaresolution.com>
2025-06-04 18:20:00 -0400

    Create APP.js

---
+++ src/app/APP.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Discover Marketplace - Enginuity UI</h1>
+      <p>Welcome to the marketplace where technology seamlessly integrates.</p>
+    </div>
+  );
+};
+
+export default App;
