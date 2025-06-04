5e47c6a2bec7f76006971346810703828c7592ad
Stephen Allen <founder@discoversoftwaresolution.com>
2025-06-04 18:44:15 -0400

    Update EnginuityDashboard.js

--- src/components/EnginuityDashboard.js
+++ src/components/EnginuityDashboard.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import EnginuityDashboard from "./components/enginuity";
 import SimulAI from "./components/simulai";
 import ProtoPrint from "./components/protoprint";
 import { computeOrbit, computeHohmannTransfer } from "../services/orbital";
