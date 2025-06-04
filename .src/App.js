5152002d80c0d81cf85639b0ef212fe59c757257
Stephen Allen <founder@discoversoftwaresolution.com>
2025-06-04 17:58:27 -0400

    Update App.js

--- .src/App.js
+++ .src/App.js
@@ -2,14 +2,10 @@ import React from "react";
 
 const App = () => {
   return (
-    <>
-      <header>
-        <h1>Discover Marketplace - Enginuity UI</h1>
-      </header>
-      <main>
-        <p>Welcome to Discover Marketplace! Your hub for innovative products.</p>
-      </main>
-    </>
+    <div>
+      <h1>Discover Marketplace - Enginuity UI</h1>
+      <p>Welcome to the marketplace where technology seamlessly integrates.</p>
+    </div>
   );
 };
 
