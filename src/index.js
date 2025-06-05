237b941b9cc5692db3c9d78fdbb18f3b965f9464
Stephen Allen <founder@discoversoftwaresolution.com>
2025-06-04 22:15:16 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import EnginuityDashboard from ."/enginuitydashboard";
+
 ReactDOM.render(
   <React.StrictMode>
     <EnginuityDashboard /> 
